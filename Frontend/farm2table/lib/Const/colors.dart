fceb9568f8609e2f4819150403b0a463dc6e7566
AlwaysFighting <87655596+AlwaysFighting@users.noreply.github.com>
2023-04-19 16:47:22 +0900

    Feat: 컬러 추가

--- Frontend/farm2table/lib/Const/colors.dart
+++ Frontend/farm2table/lib/Const/colors.dart
@@ -3,4 +3,10 @@ import 'dart:ui';
 const mainColor = Color(0xFFFFB978);
 const subColor = Color(0xFF98BD87);
 
-// const textColor =
+const textColor1 = Color(0xFF535353);
+const textColor2 = Color(0xFF8F8F8F);
+const textColor3 = Color(0xFFC4C4C4);
+
+const alarmColor = Color(0xFF4185ED);
+
+const lineColor = Color(0xFFEEEEEE);
