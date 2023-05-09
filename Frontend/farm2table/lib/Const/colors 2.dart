49ffd65cfd53515e5dc5f0bee250f72513bad505
AlwaysFighting <87655596+AlwaysFighting@users.noreply.github.com>
2023-05-09 14:49:22 +0900

    feat: google sevices get

---
+++ Frontend/farm2table/lib/Const/colors 2.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+const mainColor = Color(0xFFFFB978);
+const subColor = Color(0xFF98BD87);
+
+const textColor1 = Color(0xFF535353);
+const textColor2 = Color(0xFF8F8F8F);
+const textColor3 = Color(0xFFC4C4C4);
+
+const alarmColor = Color(0xFF4185ED);
+
+const lineColor = Color(0xFFEEEEEE);
+const subLineColor = Color(0xFFE1E1E1);
