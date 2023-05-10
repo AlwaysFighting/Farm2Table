c8064a062f9e64808171d793f7ccfd6d002ef241
ssh <victoria9981@naver.com>
2023-05-10 10:51:12 +0900

    카테고리 선택시 화면 구현완료

---
+++ Frontend/farm2table/lib/Home/Views/scrapbutton.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ScrapButton extends StatefulWidget{
+  final bool isTapped;
+  final VoidCallback onTap;
+  const ScrapButton({required this.isTapped, required this.onTap});
+
+  @override
+  State<ScrapButton> createState() => ScrapState();
+}
+
+class ScrapState extends State<ScrapButton>{
+
+
+  @override
+  Widget build(BuildContext context){
+    return GestureDetector(
+      onTapCancel: () => {widget.onTap()},
+      onTapUp: (_) => widget.onTap(),
+      child: Image.asset('assets/Images/scrap.png', width: 30,height: 30,
+      color: widget.isTapped ? Colors.red : null,
+      ),
+    );
+
+  }
+}
