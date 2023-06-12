0644e55e6ab8832813f1f4a45751441b493645d5
AlwaysFighting <zosejiri@gmail.com>
2023-06-12 20:48:43 +0900

    Feat: 마켓 완료

--- Frontend/farm2table/lib/Market/Views/market_home.dart
+++ Frontend/farm2table/lib/Market/Views/market_home.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:farm2table/Const/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -22,12 +23,21 @@ class _MarketHomePageState extends State<MarketHomePage> {
     fontWeight: FontWeight.w700,
   );
 
+  final titleTextStyle = const TextStyle(
+    color: Color(0xFF535353),
+    fontSize: 13,
+    fontWeight: FontWeight.w600,
+  );
 
   @override
   Widget build(BuildContext context) {
+
+    CollectionReference marketDetail =
+    FirebaseFirestore.instance.collection('market1');
+
     return DefaultTabController(
       initialIndex: 1,
-      length: 3,
+      length: 2,
       child: Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
@@ -52,11 +62,309 @@ class _MarketHomePageState extends State<MarketHomePage> {
         ),
         body: TabBarView(
           children: <Widget>[
-            Center(
-              child: Text("It's cloudy here"),
+            SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 12.0, left: 22.0, right: 22.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          height: 30,
+                          width: 60.0,
+                          decoration: BoxDecoration(
+                            color: mainColor,
+                            borderRadius: BorderRadius.circular(15.5),
+                          ),
+                          child: const Center(
+                              child: Text(
+                            "기본순",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          )),
+                        ),
+                        const SizedBox(width: 10.0),
+                        Container(
+                          height: 30,
+                          width: 60.0,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: mainColor,
+                              width: 1.5,
+                            ),
+                            color: null,
+                            borderRadius: BorderRadius.circular(15.5),
+                          ),
+                          child: const Center(
+                              child: Text(
+                            "인기순",
+                            style: TextStyle(
+                              color: mainColor,
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          )),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 22.0),
+                    FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                      future: marketDetail
+                          .doc('Market')
+                          .collection('Fruit')
+                          .get(),
+                      builder:
+                          (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                        if (snapshot.hasError) {
+                          return const Center(child: Text('ERROR'));
+                        }
+                        if (snapshot.connectionState == ConnectionState.waiting) {
+                          return const Center(child: CircularProgressIndicator());
+                        }
+                        if (snapshot.data == null) {
+                          return const Center(child: Text('데이터가 없습니다!'));
+                        }
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          List<QueryDocumentSnapshot> documents = snapshot.data!.docs;
+                          return GridView.builder(
+                            itemCount: documents.length,
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              crossAxisSpacing: 14.0,
+                              childAspectRatio: 0.9,
+                              mainAxisSpacing: 16.0,
+                            ),
+                            itemBuilder: (context, index) {
+                              Map<String, dynamic> data = documents[index].data() as Map<String, dynamic>;
+                              final arrays = documents.map((doc) => doc['tag'].join(' # ')).toList();
+                              return Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  boxShadow: const [
+                                    BoxShadow(
+                                      color: Color(0x25494949),
+                                      blurRadius: 4,
+                                      offset: Offset(2, 2),
+                                    ),
+                                  ],
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: const BorderRadius.only(
+                                        topLeft: Radius.circular(14.0),
+                                        topRight: Radius.circular(14.0),
+                                      ),
+                                      child: Image.network(
+                                        data['image'],
+                                        width: double.infinity,
+                                        height: 105,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 9.0),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 8.0),
+                                      child: Row(
+                                        crossAxisAlignment: CrossAxisAlignment.center,
+                                        children: [
+                                          Text(data['title'], style: titleTextStyle),
+                                          const SizedBox(width: 11.0),
+                                          SizedBox(
+                                            height : 14.0,
+                                            width: 14.0,
+                                            child: Image.asset(
+                                              "assets/Images/const/star.png",
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                          const SizedBox(width: 3.0),
+                                          Text('${data['star']}', style: titleTextStyle.copyWith(fontSize: 11.0,)),
+                                        ],
+                                      ),
+                                    ),
+                                    const SizedBox(height: 14.0),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 8.0),
+                                      child: Text(
+                                        '# ${arrays[index]}',
+                                        style: titleTextStyle.copyWith(fontSize: 12.0, fontWeight: FontWeight.w400),
+                                        softWrap: true,
+                                        maxLines: 2,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          );
+                        }
+                        return const CircularProgressIndicator();
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ),
-            Center(
-              child: Text("It's rainy here"),
+            SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 12.0, left: 22.0, right: 22.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          height: 30,
+                          width: 60.0,
+                          decoration: BoxDecoration(
+                            color: mainColor,
+                            borderRadius: BorderRadius.circular(15.5),
+                          ),
+                          child: const Center(
+                              child: Text(
+                                "기본순",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              )),
+                        ),
+                        const SizedBox(width: 10.0),
+                        Container(
+                          height: 30,
+                          width: 60.0,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: mainColor,
+                              width: 1.5,
+                            ),
+                            color: null,
+                            borderRadius: BorderRadius.circular(15.5),
+                          ),
+                          child: const Center(
+                              child: Text(
+                                "인기순",
+                                style: TextStyle(
+                                  color: mainColor,
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              )),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 22.0),
+                    FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                      future: marketDetail
+                          .doc('Market')
+                          .collection('Vegetable')
+                          .get(),
+                      builder:
+                          (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                        if (snapshot.hasError) {
+                          return const Center(child: Text('ERROR'));
+                        }
+                        if (snapshot.connectionState == ConnectionState.waiting) {
+                          return const Center(child: CircularProgressIndicator());
+                        }
+                        if (snapshot.data == null) {
+                          return const Center(child: Text('데이터가 없습니다!'));
+                        }
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          List<QueryDocumentSnapshot> documents = snapshot.data!.docs;
+                          return GridView.builder(
+                            itemCount: documents.length,
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              crossAxisSpacing: 14.0,
+                              childAspectRatio: 0.9,
+                              mainAxisSpacing: 16.0,
+                            ),
+                            itemBuilder: (context, index) {
+                              Map<String, dynamic> data = documents[index].data() as Map<String, dynamic>;
+                              final arrays = documents.map((doc) => doc['tag'].join(' # ')).toList();
+                              return Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  boxShadow: const [
+                                    BoxShadow(
+                                      color: Color(0x25494949),
+                                      blurRadius: 4,
+                                      offset: Offset(2, 2),
+                                    ),
+                                  ],
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: const BorderRadius.only(
+                                        topLeft: Radius.circular(14.0),
+                                        topRight: Radius.circular(14.0),
+                                      ),
+                                      child: Image.network(
+                                        data['image'],
+                                        width: double.infinity,
+                                        height: 105,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 9.0),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 8.0),
+                                      child: Row(
+                                        crossAxisAlignment: CrossAxisAlignment.center,
+                                        children: [
+                                          Text(data['title'], style: titleTextStyle),
+                                          const SizedBox(width: 11.0),
+                                          SizedBox(
+                                            height : 14.0,
+                                            width: 14.0,
+                                            child: Image.asset(
+                                              "assets/Images/const/star.png",
+                                              fit: BoxFit.cover,
+                                            ),
+                                          ),
+                                          const SizedBox(width: 3.0),
+                                          Text('${data['star']}', style: titleTextStyle.copyWith(fontSize: 11.0,)),
+                                        ],
+                                      ),
+                                    ),
+                                    const SizedBox(height: 14.0),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 8.0),
+                                      child: Text(
+                                        '# ${arrays[index]}',
+                                        style: titleTextStyle.copyWith(fontSize: 12.0, fontWeight: FontWeight.w400),
+                                        softWrap: true,
+                                        maxLines: 2,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          );
+                        }
+                        return const CircularProgressIndicator();
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
